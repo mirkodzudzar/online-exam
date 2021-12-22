2039d4e474fd0c5ef52d1bcce89337c5d9dab165
mirkodzudzar <mirkodzudzar@gmail.com>
2021-12-22 17:18:19 +0100

    Layout and home blades

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home page')
+    
+@section('content')
+  <h1>Home page</h1>
+  <p>Welcome to home page</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Online Exam - @yield('title')</title>
+</head>
+<body>
+  <div >
+    <h5>Online Exam</h5>
+  </div>
+  <div>
+    @yield('content')
+  </div>
+</body>
+</html>
