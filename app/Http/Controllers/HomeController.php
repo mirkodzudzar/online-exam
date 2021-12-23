b310d07875073b91d60b5f23959bfff143af840d
mirkodzudzar <mirkodzudzar@gmail.com>
2021-12-23 13:25:39 +0100

    Home controller, blade and route.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profession;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home.index', [
+            'professions' => Profession::all(),
+        ]);
+    }
+}
--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Home page')
-    
-@section('content')
-  <h1>Home page</h1>
-  <p>Welcome to home page</p>
-@endsection
