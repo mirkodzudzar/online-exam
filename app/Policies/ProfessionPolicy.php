33e4e921f37af8094a29068600dea4f099cd561d
mirkodzudzar <mirkodzudzar@gmail.com>
2021-12-27 18:45:21 +0100

    Comments for ProfessionPolicy

--- app/Policies/ProfessionPolicy.php
+++ app/Policies/ProfessionPolicy.php
@@ -10,12 +10,14 @@ class ProfessionPolicy
 {
     use HandlesAuthorization;
 
+    // Check if user already applied for some profession
     public function apply(User $user, Profession $profession)
     {
+        // If user is not candidate - admin
         if (is_null($user->candidate)) {
             return false;
         }
-
+        // Check all candidate professions to be sure if we alredy applied
         $user_professions = $user->candidate->professions()->get();
         foreach ($user_professions as $user_profession) {
             if ($user_profession->id === $profession->id) {
@@ -26,12 +28,14 @@ class ProfessionPolicy
         return true;
     }
 
+    // Check if user applied for some profession so it can be unapplied
     public function unapply(User $user, Profession $profession)
     {
+        // If user is not candidate - admin
         if (is_null($user->candidate)) {
             return false;
         }
-
+        // Check all candidate professions to be sure if we are able to unapply some profession
         $user_professions = $user->candidate->professions()->get();
         foreach ($user_professions as $user_profession) {
             if ($user_profession->id === $profession->id) {
