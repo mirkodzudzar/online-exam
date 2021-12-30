53b7d412ea8a0db58c96d429fcee258f180bdcfc
mirkodzudzar <mirkodzudzar@gmail.com>
2021-12-30 18:16:39 +0100

    Resolving duplicate policy problem

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,6 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         'App\Models\Profession' => 'App\Policies\ProfessionPolicy',
-        'App\Models\Profession' => 'App\Policies\AdminProfessionPolicy',
     ];
 
     /**
