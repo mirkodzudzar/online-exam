02901d36f7f257b691db92e9bfbc369218380960
mirkodzudzar <mirkodzudzar@gmail.com>
2022-01-17 17:48:07 +0100

    Hidding pager if there are no items

--- resources/views/components/pager.blade.php
+++ resources/views/components/pager.blade.php
@@ -1,6 +1,8 @@
-<div class="d-flex justify-content-center">
-  {{ $items->onEachSide(1)->links() }}
+@if ($items->total() > 0)
+  <div class="d-flex justify-content-center">
+    {{ $items->onEachSide(1)->links() }}
+  </div>
+  <div class="d-flex justify-content-center">
+    <p>Showing {{ $items->firstItem() }} to {{ $items->lastItem() }} of {{ $items->total() }} results</p>
 </div>
-<div class="d-flex justify-content-center">
-  <p>Showing {{ $items->firstItem() }} to {{ $items->lastItem() }} of {{ $items->total() }} results</p>
-</div>
+@endif
