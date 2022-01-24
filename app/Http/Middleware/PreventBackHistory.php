18d9c0b23d96672d95a44cda39a2a608bc053d6d
mirkodzudzar <mirkodzudzar@gmail.com>
2022-01-24 16:11:25 +0100

    FIX - minor code fix for GTM time

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -19,6 +19,6 @@ class PreventBackHistory
         $response = $next($request);
         return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
                         ->header('Pragma', 'no-cache')
-                        ->header('Expires', 'Sun, 01 Jan 1990 00:00:00 GT');
+                        ->header('Expires', 'Sun, 01 Jan 1990 00:00:00 GMT');
     }
 }
