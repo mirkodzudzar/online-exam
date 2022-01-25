ccf8c0b6ed521f716e0e0a36dce458984abed3a9
mirkodzudzar <mirkodzudzar@gmail.com>
2022-01-25 13:07:58 +0100

    Displaying all location options

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -5,15 +5,24 @@
 @section('page_title', 'List of locations')
     
 @section('content')
-  <div class="card text-center">
-    <div class="card-header bg-secondary text-white fs-5">
-      <x-geo-icon></x-geo-icon>
-      Locations
+  @if ($locations->count() > 0)
+    <div class="card text-center">
+      <div class="card-header bg-secondary text-white fs-5">
+        <x-geo-icon></x-geo-icon>
+        Locations
+      </div>
+      <ul class="list-group list-group-flush fs-4">
+        @foreach ($locations as $location)
+          <li class="list-group-item {{ $location->enabled ? '' : 'bg-warning' }}">
+            <a href="{{ route('locations.show', ['location' => $location->id]) }}" class="text-decoration-none">
+              {{ $location->name }}
+            </a>
+            @if (!$location->enabled) <x-badge value="Disabled" type="danger"></x-badge> @endif
+          </li>  
+        @endforeach
+      </ul>
     </div>
-    <ul class="list-group list-group-flush fs-4">
-      @foreach ($locations as $location)
-        <li class="list-group-item"><a href="{{ route('locations.show', ['location' => $location->id]) }}" class="text-decoration-none">{{ $location->name }}</a></li>  
-      @endforeach
-    </ul>
-  </div>
+  @else
+    <p>There are no locations yet!</p>
+  @endif
 @endsection
