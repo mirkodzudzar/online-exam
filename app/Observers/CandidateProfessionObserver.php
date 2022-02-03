f97e43f0e0d1f8bda3ddb18bb866aa48d71a46f4
mirkodzudzar <mirkodzudzar@gmail.com>
2022-02-03 18:05:27 +0100

    FIX for percentage decimal value

--- app/Observers/CandidateProfessionObserver.php
+++ app/Observers/CandidateProfessionObserver.php
@@ -11,7 +11,8 @@ class CandidateProfessionObserver
     {
         // Check value not to have division by zero.
         if ($candidate_profession->correct > 0 && $candidate_profession->total > 0) {
-            $percentage = round(($candidate_profession->correct / $candidate_profession->total) * 100, 2);
+            $percentage = ($candidate_profession->correct / $candidate_profession->total) * 100;
+            $percentage = number_format($percentage, 2, '.', '');  // two decimal places, outputs e.g. 33.33
             $candidate_profession->percentage = $percentage;
         }
     }
