b220d3cf2b359dc74ffa66b4585efb69708d40fc
mirkodzudzar <mirkodzudzar@gmail.com>
2022-02-04 00:32:57 +0100

    Adding validation for each question on exam form
    New validation is stored in event listener when we finish the exam
    - We are not using custom Request class because we need to loop through each question and validate it to be required
    - After we enter valid data, we are proceeding to save data and finish the exam
    - @TODO We still need to add functionality for exam to be filled only once per candidate, each exam can belong to many Professions, so we can not save or override data many times for one question...

--- app/Listeners/EvaluateStatusOfCandidateProfession.php
+++ app/Listeners/EvaluateStatusOfCandidateProfession.php
@@ -21,25 +21,37 @@ class EvaluateStatusOfCandidateProfession
         $correct = [];
         $wrong = [];
 
+        // First we are validating every question by specific name e.g answers15 and it is required.
+        // There are many radio buttons with different name values depending on each question id.
+        // Maybe there is some way better solution, but custom Request was not helpful right now,
+        // because we need to loop through every question and give to it reqired attribute.
         foreach ($candidate_profession->profession->exam->questions as $question) {
-            if (isset(request()->input("answers")[$question->id])) {
-                $answer = request()->input("answers")[$question->id];
-                // Counting how many answers user provided.
-                $attempted[] = $answer;
-                // Is the answer correct?
-                if ($answer === $question->answer_correct) {
-                    $correct[] = $answer;
-                // Or is it wrong?
-                } else {
-                    $wrong[] = $answer;
-                }
+            $rules["answers{$question->id}"] = 'required';
+        }
 
-                CandidateQuestion::create([
-                    'candidate_id' => $candidate_profession->candidate->id,
-                    'question_id' => $question->id,
-                    'candidate_answer' => $answer,
-                ]);
+        $validated = request()->validate($rules);
+        
+        // Once we have validated all data, we can proceed with data storing,
+        // @TODO We still need to add functionality for exam to be filled only onec per candidate,
+        // Each exam can belong to many Professions, so we can not save or override data many times for one question...
+        foreach ($candidate_profession->profession->exam->questions as $question) {
+
+            $answer = $validated["answers{$question->id}"];
+            // Counting how many answers user provided.
+            $attempted[] = $answer;
+            // Is the answer correct?
+            if ($answer === $question->answer_correct) {
+                $correct[] = $answer;
+            // Or is it wrong?
+            } else {
+                $wrong[] = $answer;
             }
+
+            $candidate_questions[] = CandidateQuestion::create([
+                'candidate_id' => $candidate_profession->candidate->id,
+                'question_id' => $question->id,
+                'candidate_answer' => $answer,
+            ]);
         }
 
         $attempted = count($attempted);
--- resources/views/includes/_profession-questions.blade.php
+++ resources/views/includes/_profession-questions.blade.php
@@ -9,21 +9,22 @@
     <div class="card mb-3 p-2 bg-light">
       <b>{{ $loop->iteration }}. {{ $question->text }}</b>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="answers[{{ $question->id }}]" id="answer_a_{{ $question->id }}" value="answer_a">
+        <input class="form-check-input" type="radio" name="answers{{ $question->id }}" id="answer_a_{{ $question->id }}" value="answer_a" {{ old("answers$question->id") === "answer_a" ? 'checked' : '' }}>
         <label class="form-check-label" for="answer_a_{{ $question->id }}">{{ $question->answer_a }}</label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="answers[{{ $question->id }}]" id="answer_b_{{ $question->id }}" value="answer_b">
+        <input class="form-check-input" type="radio" name="answers{{ $question->id }}" id="answer_b_{{ $question->id }}" value="answer_b" {{ old("answers$question->id") === "answer_b" ? 'checked' : '' }}>
         <label class="form-check-label" for="answer_b_{{ $question->id }}">{{ $question->answer_b }}</label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="answers[{{ $question->id }}]" id="answer_c_{{ $question->id }}" value="answer_c">
+        <input class="form-check-input" type="radio" name="answers{{ $question->id }}" id="answer_c_{{ $question->id }}" value="answer_c" {{ old("answers$question->id") === "answer_c" ? 'checked' : '' }}>
         <label class="form-check-label" for="answer_c_{{ $question->id }}">{{ $question->answer_c }}</label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="answers[{{ $question->id }}]" id="answer_d_{{ $question->id }}" value="answer_d">
+        <input class="form-check-input" type="radio" name="answers{{ $question->id }}" id="answer_d_{{ $question->id }}" value="answer_d" {{ old("answers$question->id") === "answer_d" ? 'checked' : '' }}>
         <label class="form-check-label" for="answer_d_{{ $question->id }}">{{ $question->answer_d }}</label>
       </div>
+      <x-error field="answers{{ $question->id }}"></x-error>
     </div>
   @endforeach
   <input type="submit" value="Finish" class="btn btn-primary" onclick="return confirm('Are you sure you want to finish applying for this profession? You can not improve your results later!')">
