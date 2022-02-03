6458cd0f1768a5f744ddefa6b0399b54f5bdf33b
mirkodzudzar <mirkodzudzar@gmail.com>
2022-02-03 13:41:54 +0100

    Resolving division by zero problem

--- app/Observers/CandidateProfessionObserver.php
+++ app/Observers/CandidateProfessionObserver.php
@@ -9,7 +9,10 @@ class CandidateProfessionObserver
 
     public function updating(CandidateProfession $candidate_profession)
     {
-        $percentage = round(($candidate_profession->correct / $candidate_profession->total) * 100, 2);
-        $candidate_profession->percentage = $percentage;
+        // Check value not to have division by zero.
+        if ($candidate_profession->correct > 0 && $candidate_profession->total > 0) {
+            $percentage = round(($candidate_profession->correct / $candidate_profession->total) * 100, 2);
+            $candidate_profession->percentage = $percentage;
+        }
     }
 }
