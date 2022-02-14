29c0d150639404d1a132a5fbf6775da6946a609c
mirkodzudzar <mirkodzudzar@gmail.com>
2022-02-14 17:01:38 +0100

    Result page for professions exam restrictions
    Adding CandidateProfessionPolicy method for results

--- app/Policies/CandidateProfessionPolicy.php
+++ app/Policies/CandidateProfessionPolicy.php
@@ -26,29 +26,6 @@ class CandidateProfessionPolicy
     }
 
     /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\CandidateProfession  $candidateProfession
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, CandidateProfession $candidateProfession)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
@@ -75,39 +52,13 @@ class CandidateProfessionPolicy
         return true;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\CandidateProfession  $candidateProfession
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, CandidateProfession $candidateProfession)
+    public function results(User $user, CandidateProfession $candidateProfession)
     {
-        //
-    }
+        // If user is not candidate - admin
+        if (is_null($user->candidate)) {
+            return false;
+        }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\CandidateProfession  $candidateProfession
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, CandidateProfession $candidateProfession)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\CandidateProfession  $candidateProfession
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, CandidateProfession $candidateProfession)
-    {
-        //
+        return true;
     }
 }
